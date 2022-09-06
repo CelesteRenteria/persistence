501cd8bd22896f21b2043429cf9078c849ad619d
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2022-09-06 18:27:37 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:persistence/screens/home_screen.dart';
+
+void main() {
+  runApp(const SqliteApp());
+}
+
+class SqliteApp extends StatelessWidget {
+  const SqliteApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "SQLite Example",
+      initialRoute: "home",
+      routes: {"home": (context) => const HomeScreen()},
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: const AppBarTheme(color: Colors.amber)),
+    );
+  }
+}
