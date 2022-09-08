eb1ee878d7c964709776ee8dc5c9e400aed14a4a
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2022-09-08 18:41:24 -0500

    quien sabe si este bien

--- lib/models/cat_model.dart
+++ lib/models/cat_model.dart
@@ -1,19 +1,16 @@
 class Cat {
-  //int id;
+  //int id
+  var id;
   final String race;
   final String name;
 
 //no importa el orden en el que se pasen los parametros
-  //required this.id,
-  Cat({required this.race, required this.name});
+  //required this.id, ver como le puedo quitar el id
+  Cat({required this.race, required this.name, this.id});
 
   factory Cat.fromMap(Map<String, dynamic> json) =>
-      Cat(
-        //id: json['id'], 
-        race: json['race'], name: json['name']);
+      Cat(id: json['id'], race: json['race'], name: json['name']);
   Map<String, dynamic> toMap() {
-    return {
-      //'id': id, 
-      'race': race, 'name': name};
+    return {'id': id, 'race': race, 'name': name};
   }
 }
